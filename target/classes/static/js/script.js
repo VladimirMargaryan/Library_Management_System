c515be13fd58c840bac4a78b9fd01a64950597c9
Your Name <vladimir_margaryan@edu.aua.am>
2022-02-08 08:41:51 +0400

    notification bell

--- target/classes/static/js/script.js
+++ target/classes/static/js/script.js
@@ -6,12 +6,10 @@ function toggleNotify(){
     if (down) {
         box.style.height  = '0px';
         box.style.opacity = '0';
-        // box.style.display = 'none';
         down = false;
     }else {
         box.style.height  = '510px';
         box.style.opacity = '1';
-        // box.style.display = 'inline-block';
         down = true;
     }
 }
